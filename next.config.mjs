47b700d2d6c78357518b97dbfa171e8b1c84662f
StayVistaTest <stayvista.test@gmail.com>
2026-08-21 10:01:04 +0000

    Remove sharp, fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis'],
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'sharp'];
+    return config;
+  },
+};
 export default nextConfig;
